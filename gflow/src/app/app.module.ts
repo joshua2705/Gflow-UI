7c7fae68c5c1b195e7e3886ac0c857fa359ed525
Alexander,Joshua <joshua.alexander2705@gmail.com>
2022-11-08 21:33:37 +0530

    NavBar UI

--- gflow/src/app/app.module.ts
+++ gflow/src/app/app.module.ts
@@ -4,7 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
+
+import { MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu'
+import { MatButtonModule } from '@angular/material/button'
+import {MatDividerModule} from '@angular/material/divider';
+
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
@@ -36,7 +42,11 @@ import { AuthorityHomepageComponent } from './authority-homepage/authority-homep
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
